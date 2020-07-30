cd04a68763caad232fd3714f789c26f0a5c47a96
theaustinator <austinmckay303@gmail.com>
2020-07-30 12:36:04 -0600

    started datatree test

---
+++ tests/test_data_tree.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.fixture
+def tree_spec():
+    # for a given process, either matched arrays for everything or  that sweep everything, or
+    tree_spec = [{"process": "normalize", "params": [{}, {}], "subset": [{""}]}, {}]
