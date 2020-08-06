ce3a196807514c074f3389da52303ecf98638247
Munchic <khoi.u.pham@gmail.com>
2020-08-06 00:29:24 -0300

    better name for cellforest_load test :)

--- tests/test_cellforestR.py
+++ tests/test_cellforestR.py
@@ -5,7 +5,7 @@ from cellforest.utils.r.run_r_script import run_r_script
 from tests.fixtures import *
 
 
-def test_cellforestR(root_path_example, norm_spec, random_process):
+def test_cellforest_load(root_path_example, norm_spec, random_process):
     path_to_cellforestR_script = Path(__file__).parent / "r" / "cellforest_load.R"
     # TO-DO: Add support for passing spec through Rscript
     run_r_script(path_to_cellforestR_script, [root_path_example, "placeholder", random_process])
