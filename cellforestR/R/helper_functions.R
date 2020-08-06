881ae56b2294eda257ed9ebcb580277c5c97f931
Munchic <khoi.u.pham@gmail.com>
2020-08-05 23:42:04 -0300

    comment :)

--- cellforestR/R/helper_functions.R
+++ cellforestR/R/helper_functions.R
@@ -21,6 +21,7 @@ processes_between_paths <- function(path_upstream, path_downstream) {
 
   processes_and_hashes <- strsplit(path_diff, "/")[[1]]
   processes <- c()
+  # get process name and skip hashes
   for (i in 1:length(processes_and_hashes)) {
     if ((i %% 2) == 1) {
       processes <- append(processes, processes_and_hashes[i])
