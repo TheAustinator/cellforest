02c0c7e8fe6344a3d591697e7a3f4978e82ca8c2
Munchic <khoi.u.pham@gmail.com>
2020-08-19 12:01:01 -0300

    reduce proper kwargs

--- cellforest/plot/r/plot_umap_embeddings_scat.R
+++ cellforest/plot/r/plot_umap_embeddings_scat.R
@@ -2,7 +2,9 @@ source('cellforest/plot/r/plot_entry_point.R')
 
 library(ggforce)
 
-embeddings <- data.frame(seurat_obj$umap@cell.embeddings[, c(1:ifelse(is.null(kwargs$npcs), 2, kwargs$npcs))])
+default_npcs <- 2
+
+embeddings <- data.frame(seurat_obj$umap@cell.embeddings[, c(1:ifelse(is.null(kwargs$npcs), default_npcs, kwargs$npcs))])
 if (!is.null(kwargs$group.by)) {
     embeddings[[kwargs$group.by]] <- seurat_obj@meta.data[match(row.names(embeddings), row.names(seurat_obj@meta.data)), kwargs$group.by]
 }
@@ -15,7 +17,7 @@ ggplot(embeddings, aes_string(x = ".panel_x", y = ".panel_y", fill = kwargs$grou
     ) + 
     geom_autodensity(alpha = 0.3, colour = NA, position = "identity") +
     facet_matrix(
-        vars(names(embeddings[c(1:ifelse(is.null(kwargs$npcs), 5, kwargs$npcs))])),
+        vars(names(embeddings[c(1:ifelse(is.null(kwargs$npcs), default_npcs, kwargs$npcs))])),
         layer.diag = 2
     )
 
